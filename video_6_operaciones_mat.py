42fe6650a7971513d4b84d6865c5af7a01ee72fc
reyfico <federicodesimone30@gmail.com>
2021-06-06 14:45:50 -0300

    nose3

---
+++ video_6_operaciones_mat.py
@@ -0,0 +1,27 @@
+num1 = 10
+num2 = 5
+
+resultado = num1 + num2
+print(resultado)
+
+resultado = num1 - num2
+print(resultado)
+
+resultado = num1 * num2
+print(resultado)
+
+resultado = num1 / num2
+print(resultado)
+
+resultado = num1 // num2
+print(resultado)
+
+resultado = num1 ** num2
+print(resultado)
+
+resultado = num1 % num2  ### modulo
+print(resultado)
+
+print("operacion loca")
+resultado = 3**3 * (13/5 - (2*4))
+print(resultado)
