98828dba45486a6880a8bd8e0f5ef384c0e4ef83
reyfico <federicodesimone30@gmail.com>
2021-06-06 14:11:24 -0300

    nose

--- video_4.py
+++ video_4.py
@@ -1,4 +1,17 @@
-numero = 10
+numero = 10,5
 
 print("hola mundo")
 print(numero)
+print(type(numero))
+
+cadena = "Estoy 'estudiando'"
+
+print(cadena)
+print(type(cadena))
+
+valor = True
+
+print(valor)
+print(type(valor))
+
+
