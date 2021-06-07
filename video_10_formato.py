490d8e9866e253b817bb51a985be618995c78e81
reyfico <federicodesimone30@gmail.com>
2021-06-07 01:13:32 -0300

    video 11

---
+++ video_10_formato.py
@@ -0,0 +1,10 @@
+nombre = "fede"
+edad = 40
+
+print("Hola {} tienes {} años de edad".format(nombre,edad))
+
+print(f"Hola {nombre} tienes {edad} años de edad")
+
+
+
+
---
+++ video_8_operadores_log.py
@@ -0,0 +1,7 @@
+a=10
+b=12
+c=13
+d=10
+
+nose = not(((a>b) or (a<c)) and ((a==c) or (a>=b)))
+print(nose)
